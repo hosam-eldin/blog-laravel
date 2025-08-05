217897ce23b1667d8d8db1c33b706a7a8917082f
hosam-eldin <h.eldinmoh95@gmail.com>
2025-08-05 15:04:07 +0300

    create tamplates.app file

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,33 +1,11 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+@section('title')
+    show
+@endsection
 
-    <title>show</title>
-  </head>
-  <body>
-
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">hosam Blog</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{route('posts.index')}}">All Posts</a>
-              </li>
-            </ul>
-            
-          </div>
-        </div>
-      </nav>
+@section('content')
+    
 
       <div class="m-5">
         <div class="card mt-3 ">
@@ -52,7 +30,7 @@
       </div>
 
 
+@endsection
+
   
 
-  </body>
-</html>
