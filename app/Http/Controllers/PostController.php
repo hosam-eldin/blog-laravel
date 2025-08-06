d7e2befeac90b50b456d1dc0ad9bbf60a95a8f49
hosam-eldin <h.eldinmoh95@gmail.com>
2025-08-06 09:57:41 +0300

    finish crud orders --frist one

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,4 +23,44 @@ class PostController extends Controller
         $singlePost = ['id' => 1 , 'title' => 'php' ,'text' => 'hello world' , 'posted_by' => 'hosam' , 'created_at' => '22-10-10'];
         return view('show', ['post' => $singlePost]);
     }
+
+    public function create(){
+        
+        return view('create');
+    }
+
+    public function edit(){
+        
+        return view('edit');
+    }
+
+    public function store(){
+        
+       $data = request()->all();
+
+       $title = request()->title;
+       $content = request()->content;
+       $posted_by = request()->posted_by;
+       
+        return redirect()->route('posts.index');
+
+    }
+
+    public function update(){
+
+        $new_title = request()->title;
+       $new_content = request()->content;
+       $new_posted_by = request()->posted_by;
+       
+        return redirect()->route('posts.show',1);
+
+    }
+
+     public function destroy(){
+        
+        return redirect()->route('posts.index');
+    }
+
+
+
 }
